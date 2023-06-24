5c5b69d42c026d715d0196a8395ea61f21087374
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-06-24 18:24:06 -0300

    fonte local

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -4,27 +4,29 @@ import vistas from '../../assets/icones/mais-vistas-inativo.png'
 import novas from '../../assets/icones/novas-inativo.png'
 import suspresa from '../../assets/icones/surpreenda-me-inativo.png'
 
+import styles from './Menu.module.scss'
+
 const Menu = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className={styles.menu}>
+      <ul className={styles.menu__lista}>
+        <li className={styles.menu__item}>
           <img src={home} alt="home"/>
           <a href="/">Início</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={curtidas} alt="curtidas"/>
           <a href="/">Mais curtidas</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={vistas} alt="vistas"/>
           <a href="/">Mais vistas</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={novas} alt="novas"/>
           <a href="/">Mais novas</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={suspresa} alt="supresa"/>
           <a href="/">Surpresa</a>
         </li>
